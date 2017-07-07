6fc91373da8e638c377ae110ccf954aef9cc8fed
Kyle Farnung <kfarnung@microsoft.com>
2017-07-07 16:06:38 -0700

    Added a copy button and reformatted a bit

--- review.js
+++ review.js
@@ -108,17 +108,34 @@ if (typeof browser !== 'undefined') {
   getCollaborators((err, collabs) => {
     if (err) {
       const str = `
-      <p>
-        <strong>Error: Something went wrong</strong>
-        <br>Unable to load collaborators
-      </p>`
+        <p>
+          <strong>Error: Something went wrong</strong>
+          <br>Unable to load collaborators
+        </p>`
       showMessage(str, 'error')
       console.error('Unable to load collaborators', err)
       return
     }
 
     const out = formatMeta(meta, collabs)
-    showMessage(`<strong>PR Metadata</strong><br>${out}`)
+    showMessage(`
+      <h3>PR Metadata</h3>
+      <div style="margin-top:0.5em">
+        <div>
+          <div style="float:right">
+            <div class="BtnGroup">
+              <button aria-label="Copy the PR metadata" class="js-zeroclipboard btn btn-outline BtnGroup-item zeroclipboard-button tooltipped tooltipped-s" data-clipboard-text="${out}" data-copied-hint="Copied!" type="button">
+                <svg aria-hidden="true" class="octicon octicon-clippy" height="16" version="1.1" viewBox="0 0 14 16" width="14">
+                  <path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div>
+            <pre>${out}</pre>
+          </div>
+        </div>
+      </div>`)
   })
 
   function getPR () {
@@ -210,26 +227,21 @@ if (typeof browser !== 'undefined') {
 
     const { refs, fixes } = getRefsAndFixes()
 
-    const result = [`PR-URL: ${prUrl}<br>`]
+    const result = [`PR-URL: ${prUrl}`]
 
     if (fixes && fixes.length) {
       fixes.forEach((fix) => {
-        result.push(`Fixes: ${fix}<br>`)
+        result.push(`Fixes: ${fix}`)
       })
     }
 
     if (refs && refs.length) {
       refs.forEach((ref) => {
-        result.push(`Refs: ${ref}<br>`)
+        result.push(`Refs: ${ref}`)
       })
     }
 
-    return `<br>
-    <p>
-      ${result.join('\n')}
-      ${revs.join('<br>')}<br>
-    </p>
-    `
+    return result.join('\n') + '\n' + revs.join('\n')
   }
 
   function getClassForType (type = 'info') {
